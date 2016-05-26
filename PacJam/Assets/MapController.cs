1b1247d0a9d0de9543ae374dcaf07fdc6bb23cda
shuff264 <jshuff264@gmail.com>
2016-05-26 13:04:32 +0100

    Base commit
    Also base map in

---
+++ PacJam/Assets/MapController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+
+	public GameObject mapTile;
+	int mapSizeX = 10;
+	int mapSizeY = 10;
+
+	// Use this for initialization
+	void Awake () {
+
+		BuildMap ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void BuildMap(){
+		for (int i = 0; i < mapSizeX; i++) {
+			for (int j = 0; j < mapSizeY; j++) {
+				GameObject obj = (GameObject)Instantiate (mapTile, new Vector3 (i, j, 0), Quaternion.identity);
+				obj.transform.SetParent (this.gameObject.transform);
+			}
+		}
+	}
+}
---
+++ PacJam/Assets/Tile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public GameObject highlight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//TODO: If it is placeable be green, if not be red
+	void OnMouseEnter(){
+		highlight.SetActive (true);
+	}
+
+	void OnMouseExit(){
+		highlight.SetActive (false);
+	}
+}
