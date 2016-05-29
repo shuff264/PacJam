7547eb839c9fce16d99765e153bf2891e9574966
shuff264 <jshuff264@gmail.com>
2016-05-29 16:23:46 +0100

    Base Player Money

---
+++ PacJam/Assets/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public Text moneyText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moneyText.text = "MONEY: £" + GameManager.instance.ReturnPlayerMoney().ToString();	
+	}
+}
